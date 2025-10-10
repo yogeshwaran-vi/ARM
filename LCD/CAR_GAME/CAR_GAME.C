7533a4e6c5e42df6a40ed517c71a10392a67c5d6
Yogeshwaran <yogeshofc42@gmail.com>
2025-10-10 18:03:57 +0530

    CAR_GAME.C

--- LCD/CAR_GAME/CAR_GAME.C
+++ LCD/CAR_GAME/CAR_GAME.C
@@ -1,15 +1,18 @@
 #include <LPC21XX.h>
 #include "lcd_header.h"
+
+#define interrupt pin 16
+
 unsigned int i,j,count=0;
 unsigned char a=0xC3,b=0x86,c=0xCA,d=0x8D,x=0x80,y=0,obj;
+
 void cgram_car(void);
 void cgram_obj(void);
 void game_over(void);
 void ext_int0_isr(void) __irq;
-//void ext_int1_isr(void) __irq;
-unsigned char cgram_car_sym[]={0x00,0X0E,0X1F,0X1F,0X1F,0X0A,0X0A,0X00};
-unsigned char cgram_obj_sym[]={0x07,0x08,0x07,0x08,0x07,0x08,0x07,0x00};
 
+unsigned char cgram_car_sym[]={0x00,0X0E,0X1F,0X1F,0X1F,0X0A,0X0A,0X00};
+unsigned char cgram_obj_sym[]={0x00,0x00,0x04,0x08,0x1F,0x08,0x04,0x00};
 
 int main()
 {
@@ -20,23 +23,23 @@ int main()
 	delay_ms(500);
 
 	lcd_cmd(0xC8);
-	lcd_data('3');
+	lcd_string("03");
 	delay_ms(500);
 
 	lcd_cmd(0xC8);
-	lcd_data('2');
+	lcd_string("02");
 	delay_ms(500);
 
 	lcd_cmd(0xC8);
-	lcd_data('1');
+	lcd_string("01");
 	delay_ms(500);
 
 	lcd_cmd(0xC8);
-	lcd_data("GO");
+	lcd_string("GO");
 	delay_ms(500);
 
 	cgram_car();
-	lcd_cmd(0xC0); 					//STARTING ADDRESS OF LCD
+	lcd_cmd(0xC0); 						//STARTING ADDRESS OF LCD
 	cgram_obj();
 	cgram_obj();
 	cgram_obj();
@@ -46,8 +49,6 @@ int main()
 	
 	VICVectCntl0 = (0x20)|14; 			//EXTINT NUMBER 14
 	VICVectAddr0 = (unsigned long)ext_int0_isr;
-	//VICVectCntl1 = (0X20)|15;			//EXTINT NUMBER 15
-	//VICVectAddr1 = (unsigned long)ext_int1_isr;
 
 	EXTMODE = 0x01;   					//EDGE SENSITIVE
 	EXTPOLAR = 0x00;  					//FALLING EDGE-SENSITIVITY
@@ -112,9 +113,7 @@ int main()
 		else if(obj==c)
 		  	game_over();
 		else if(obj==d)
-		  	game_over();
-			
-			
+		  	game_over();		
 	}
 }
 
@@ -122,8 +121,7 @@ int main()
 void cgram_car(void)
 {
 	char i=0;
-	//lcd_cmd(x);
-	lcd_cmd(0x40);						//STARTING ADDRESS OF CGRAM
+	lcd_cmd(0x40);						//DATA 0 STARTING ADDRESS OF CGRAM
 	for(i=0;i<8;i++)
 	{
 		lcd_data(cgram_car_sym[i]);
@@ -134,20 +132,14 @@ void cgram_car(void)
 void cgram_obj(void)
 {
 	char i=0;
-	lcd_cmd(0x48);						//STARTING ADDRESS OF CGRAM
+	lcd_cmd(0x48);						//NEXT DATA 1 ADDRESS OF CGRAM
 	for(i=0;i<8;i++)
 	{
 		lcd_data(cgram_obj_sym[i]);
 	}
 }
 
-//***********EXINT0-ISR**********//
-void ext_int0_isr(void) __irq
-{
-	EXTINT = 0x01;  					//reset flag 
-	    y++;
-	VICVectAddr=0; 
-}
+//***********GAME OVER**********//
 void game_over(void)
 {
 	lcd_cmd(0x01);
@@ -159,6 +151,13 @@ void game_over(void)
 		lcd_integer(count);
 	}
 }
-//*********END OF ISR********//	
-	 
 
+//***********EXINT0-ISR**********//
+void ext_int0_isr(void) __irq
+{
+	EXTINT = 0x01;  					//reset flag 
+	    y++;
+	VICVectAddr=0; 
+}
+//*********END OF ISR********//
+//****************************************THE END************************************************//
